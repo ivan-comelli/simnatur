af9b80eb9c7f50d4a5469768641d0b3b8a23aa6c
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-01 18:06:26 -0300

    new

--- server/models/index.js
+++ server/models/index.js
@@ -2,12 +2,7 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')['production'];
 console.log(config)
-let sequelizeDB;
-if (config.use_env_variable) {
-  sequelizeDB = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelizeDB = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelizeDB = new Sequelize(config.database, config.username, config.password, config);
 const User = require('./user')(sequelizeDB, Sequelize.DataTypes);
 const Product = require('./product')(sequelizeDB, Sequelize.DataTypes);
 const BlacklistedToken = require('./blacklistedtoken')(sequelizeDB, Sequelize.DataTypes);
