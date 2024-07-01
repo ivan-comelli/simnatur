48f4132d40450333f249d4989b60dc8959ae86b3
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-01 18:42:53 -0300

    try

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
-import pg from "pg"
-
-module.exports = {
+const pg = require("pg");
+export default {
   development: {
     username: "root",
     password: null,
@@ -17,7 +16,7 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: true // Ajusta esto según tu configuración de SSL
+        rejectUnauthorized: false // Ajusta esto según tu configuración de SSL
       }
     },
     host: "ep-patient-wind-a5pxrj6x.us-east-2.aws.neon.tech",
@@ -26,5 +25,11 @@ module.exports = {
     password: "2Uq4vtcoLeYx",
     database: "neondb",
     dialectModule: pg,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000, // Tiempo máximo en milisegundos que el pool intentará adquirir una conexión antes de lanzar un error
+    idle: 10000
   }
-};
+}
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,42 @@
 const Sequelize = require('sequelize');
+const pg = require("pg")
+
+
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')['production'];
+const config = {
+  development: {
+    username: "root",
+    password: null,
+    database: "database_shop_simnatur",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: process.env.DATABASE_URL || ':memory:'
+  },
+  production: {
+    dialect: 'postgres', // Cambiar a PostgreSQL en producción
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Ajusta esto según tu configuración de SSL
+      }
+    },
+    host: "ep-patient-wind-a5pxrj6x.us-east-2.aws.neon.tech",
+    port: "5432",
+    username: "neondb_owner",
+    password: "2Uq4vtcoLeYx",
+    database: "neondb",
+    dialectModule: pg,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000, // Tiempo máximo en milisegundos que el pool intentará adquirir una conexión antes de lanzar un error
+    idle: 10000
+  }
+}['production']
 console.log(config)
 const sequelizeDB = new Sequelize(config.database, config.username, config.password, config);
 const User = require('./user')(sequelizeDB, Sequelize.DataTypes);
