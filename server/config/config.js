c9e867e9da7299ec7ef229e9c7eec3bf12a518fa
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-02 19:53:39 -0300

    new

--- server/config/config.js
+++ server/config/config.js
@@ -23,9 +23,9 @@ const config = {
     },
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     dialectModule: pg,
   },
   pool: {
