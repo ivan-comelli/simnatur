bce8b1047d099147d9176b63b92848fcbedcd1f3
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-05 19:01:26 -0300

    Checkout

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -121,7 +121,7 @@
                                 </div>
                             </div>
                             <div class="place-order mt-25">
-                                <button @click="goPayment" class="btn-hover">Pagar Orden</button>
+                                <div id="paymentBrick_container"></div>
                             </div>
                         </div>
                     </div>
@@ -173,15 +173,64 @@
         mounted() {
             const mp = new MercadoPago('APP_USR-f43919a5-4ac6-499e-a6a3-8f1f555975c5');
             const bricksBuilder = mp.bricks();
-            this.paymentCart().then(()=> {
-                bricksBuilder.create("wallet", "wallet_container", {
+            const renderPaymentBrick = async (bricksBuilder) => {
+                const settings = {
                     initialization: {
+                        /*
+                        "amount" es el monto total a pagar por todos los medios de pago con excepción de la Cuenta de Mercado Pago y Cuotas sin tarjeta de crédito, las cuales tienen su valor de procesamiento determinado en el backend a través del "preferenceId"
+                        */
+                        amount: 100,
                         preferenceId: this.preference.id,
-                        redirectMode: "blank"
-
-                    }
-                });
-            });
+                    },
+                    customization: {
+                        paymentMethods: {
+                        ticket: "all",
+                        creditCard: "all",
+                        debitCard: "all",
+                        mercadoPago: "all",
+                        },
+                    },
+                    callbacks: {
+                        onReady: () => {
+                        /*
+                            Callback llamado cuando el Brick está listo.
+                            Aquí puede ocultar cargamentos de su sitio, por ejemplo.
+                        */
+                        },
+                        onSubmit: ({ selectedPaymentMethod, formData }) => {
+                        // callback llamado al hacer clic en el botón enviar datos
+                        return new Promise((resolve, reject) => {
+                            fetch("/process_payment", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(formData),
+                            })
+                            .then((response) => response.json())
+                            .then((response) => {
+                                // recibir el resultado del pago
+                                resolve();
+                            })
+                            .catch((error) => {
+                                // manejar la respuesta de error al intentar crear el pago
+                                reject();
+                            });
+                        });
+                        },
+                        onError: (error) => {
+                        // callback llamado para todos los casos de error de Brick
+                        console.error(error);
+                        },
+                    },
+                };
+                window.paymentBrickController = await bricksBuilder.create(
+                    "payment",
+                    "paymentBrick_container",
+                    settings
+                );
+            };
+            renderPaymentBrick(bricksBuilder);
         },
         head() {
             return {
