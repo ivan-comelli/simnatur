3b8590a9e976c20063b89c40791f3f844600a5cc
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-06 19:50:11 -0300

    new

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,8 @@ module.exports = {
     ],
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'Flone - VueJS eCommerce Template',
-        titleTemplate: 'Flone | %s',
+        title: 'Simnatur - Primera Calidad de Productos Naturales',
+        titleTemplate: 'Simnatur | %s',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
