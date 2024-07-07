b44c7cafb1990ba9ec7c7086ead96dee299e80a1
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-07 16:27:57 -0300

    new

--- static/sw.js
+++ static/sw.js
@@ -1,22 +1,64 @@
+function generateCacheVersion() {
+  const filesToHash = [
+    '/js/main.js',
+    '/css/style.css',
+    // Agrega otros recursos clave aquí
+  ];
+
+  const hash = filesToHash.reduce((acc, file) => {
+    return acc + file;
+  }, '');
+
+  return 'my-cache-' + hash;
+}
+
+const CACHE_NAME = generateCacheVersion();
+
+navigator.serviceWorker.addEventListener('controllerchange', () => {
+  console.log('Se ha activado una nueva versión del service worker');
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage({ type: 'clearLocalStorage' });
+  }
+});
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/css/style.css',
+        '/js/main.js'
+        // Agregar otros recursos estáticos aquí
+      ]);
+    })
+  );
+});
 
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-
-    event.waitUntil(
-      caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            console.log(cacheName)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    // Limpiar localStorage
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage({ type: 'clearLocalStorage' });
+      });
+    }),
+    // Eliminar cachés antiguas
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
-          })
-        );
-      })
-    );
-    return self.clients.claim();
-  });
-  
+          }
+        })
+      );
+    })
+  );
+});
+
+// Escuchar mensajes del cliente para limpiar localStorage
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'clearLocalStorage') {
+    console.log('Limpiando localStorage...');
+    localStorage.clear();
+  }
+});
