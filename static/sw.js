d9d1ba2d3fe4625a16566ce61c442fa5b1fc7f5d
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-07 17:17:54 -0300

    new

--- static/sw.js
+++ static/sw.js
@@ -1,39 +1,6 @@
-function generateCacheVersion() {
-  const filesToHash = [
-    '/js/main.js',
-    '/css/style.css',
-    // Agrega otros recursos clave aquí
-  ];
-
-  const hash = filesToHash.reduce((acc, file) => {
-    return acc + file;
-  }, '');
-
-  return 'my-cache-' + hash;
-}
-
-const CACHE_NAME = generateCacheVersion();
-
-navigator.serviceWorker.addEventListener('controllerchange', () => {
-  console.log('Se ha activado una nueva versión del service worker');
-  if (navigator.serviceWorker.controller) {
-    navigator.serviceWorker.controller.postMessage({ type: 'clearLocalStorage' });
-  }
-});
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/css/style.css',
-        '/js/main.js'
-        // Agregar otros recursos estáticos aquí
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
-
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     // Limpiar localStorage
@@ -46,9 +13,7 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          return caches.delete(cacheName);
         })
       );
     })
