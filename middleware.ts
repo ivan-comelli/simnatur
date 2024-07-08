b1e4dab7e696e3eef011ce1641337b607295304b
ivan-comelli <ivancomelli.99@gmail.com>
2024-07-08 18:41:09 -0300

    new

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,15 @@ import { NowRequest, NowResponse } from '@vercel/node';
 
 
 export default async function handler(req: NowRequest, res: NowResponse) {
+  console.log('Request received:', req.url);
+  
   // Aplicar headers para controlar la caché
   res.setHeader('Cache-Control', 'public, max-age=3600');
+  
   // Manejar la respuesta según la lógica del middleware
   res.end('Hello from middleware!');
+
+  console.log('Response sent:', res.statusCode);
+  console.log("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  
 }
