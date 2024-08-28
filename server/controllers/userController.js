c570f3401e8f928ecff51d16890b71be0a3b19d0
ivan-comelli <ivancomelli.99@gmail.com>
2024-08-28 14:34:39 -0300

    new

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,28 +9,28 @@ const getUsers = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-const updatePersonalDataUser = async (req, res) => {
-  const { name, lastName, birtday, phone, document } = req.body;
-  try {
-    //SOLO SE PUEDE ACTUALIZAR SI NO EXISTEN LOS DATOS
-    //EVENTUALMENTE VA A VER PROTOCOLO PARA CAMBIAR CIERTOS DATOS
-    const user = await User.findByPk(req.userId);
-    user.name = 
-    user.lastName =
-  } catch (error) {
-
-  }
-}
-
-const updateAddressBookUser = async (req, res) => {
-  const { street, city, detail, cp } = req.body;
-  try {
-    const user = await User.findByPk(req.userId);
-    
-  } catch (error) {
-
-  }
-}
+//
+//const updatePersonalDataUser = async (req, res) => {
+// const { name, lastName, birtday, phone, document } = req.body;
+//  try {
+//    //SOLO SE PUEDE ACTUALIZAR SI NO EXISTEN LOS DATOS
+//    //EVENTUALMENTE VA A VER PROTOCOLO PARA CAMBIAR CIERTOS DATOS
+//    const user = await User.findByPk(req.userId);
+//    user.name = 
+//    user.lastName =
+//  } catch (error) {
+//
+//  }
+//}
+//
+//const updateAddressBookUser = async (req, res) => {
+//  const { street, city, detail, cp } = req.body;
+//  try {
+//    const user = await User.findByPk(req.userId);
+//    
+//  } catch (error) {
+//
+//  }
+//}
 
 module.exports = { getUsers };
