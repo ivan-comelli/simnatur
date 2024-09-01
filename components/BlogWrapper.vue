57cee1d1ab654e86a44e26d25b47c3727439eea9
ivan-comelli <ivancomelli.99@gmail.com>
2024-09-01 18:54:13 -0300

    new

--- components/BlogWrapper.vue
+++ components/BlogWrapper.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="blog-area pb-55">
-        <div class="container">
+        <div class="container-fluid">
             <SectionTitleWithSubTitle title="NUESTROS ARTICULOS" classes="section-title mb-55" />
             <div class="row">
-                <div class="col-lg-3 col-sm-5 col-10" v-for="(blog, index) in blogData.slice(0, 3)" :key="index">
+                <div v-for="(blog, index) in blogData.slice(0, 3)" :key="index">
                     <BlogItem :blog="blog" />
                 </div>
             </div>
