7464e47734edb1fb63c6bcdac948650529a8e1ab
ivan-comelli <ivancomelli.99@gmail.com>
2024-09-01 19:01:17 -0300

    new

--- components/SectionTitleWithSubTitle.vue
+++ components/SectionTitleWithSubTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center" :class="classes">
+    <div class="text-center mobil-side-text" :class="classes">
         <h2>{{ title }}</h2>
         <p>{{ subTitle }}</p>
     </div>
