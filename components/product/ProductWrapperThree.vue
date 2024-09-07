f4b1dde04b531b34f5f7125755efba43255c389d
ivan-comelli <ivancomelli.99@gmail.com>
2024-09-07 19:09:45 -0300

    neqw

--- components/product/ProductWrapperThree.vue
+++ components/product/ProductWrapperThree.vue
@@ -7,51 +7,17 @@
           subTitle="Tenemos en cuenta las necesidades de tu mascota y las tratamos con la mejor calidad y naturalidad" 
         />
         <div class="custom-row adjust-row" v-if="products && products.length">
-          <section class="embla">
-            <div class="embla__viewport">
+          <div>
+            <div ref="emblaRef" class="embla">
               <div class="embla__container">
-                <div class="embla__slide">
-                  <div class="embla__slide__number">1</div>
-                </div>
-                <div class="embla__slide">
-                  <div class="embla__slide__number">2</div>
-                </div>
-                <div class="embla__slide">
-                  <div class="embla__slide__number">3</div>
-                </div>
-                <div class="embla__slide">
-                  <div class="embla__slide__number">4</div>
-                </div>
-                <div class="embla__slide">
-                  <div class="embla__slide__number">5</div>
+                <div class="embla__slide" v-for="(color, i) in colors" :key="i" :style="{ backgroundColor: color }">
+                  Slide {{ i }}
                 </div>
               </div>
             </div>
-
-            <div class="embla__controls">
-              <div class="embla__buttons">
-                <button class="embla__button embla__button--prev" type="button">
-                  <svg class="embla__button__svg" viewBox="0 0 532 532">
-                    <path
-                      fill="currentColor"
-                      d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
-                    ></path>
-                  </svg>
-                </button>
-
-                <button class="embla__button embla__button--next" type="button">
-                  <svg class="embla__button__svg" viewBox="0 0 532 532">
-                    <path
-                      fill="currentColor"
-                      d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-
-              <div class="embla__dots"></div>
-            </div>
-          </section>
+            <button @click="scrollPrev">Prev</button>
+            <button @click="scrollNext">Next</button>
+          </div>
           <div class="custom-col-5" v-for="(product, index) in products.slice(0, 10)" :key="product.id">
             <ProductGridItemTwo :product="product" />
           </div>
@@ -63,8 +29,14 @@
   
   <script>
   import { mapGetters, mapActions } from 'vuex';
-  
+  import EmblaCarousel from 'embla-carousel'
+
   export default {
+    data() {
+      return {
+        emblaApi: null,
+      }
+    },
     components: {
       ProductGridItemTwo: () => import('@/components/product/ProductGridItemTwo'),
       QuickView: () => import('@/components/QuickView'),
@@ -72,7 +44,13 @@
     },
     computed: {
       ...mapGetters('products', ['getProducts']),
-  
+      
+      colors() {
+        return Array.from({ length: 20 }, (_, i) => {
+          const hue = (i * 137.508) % 360;
+          return `hsl(${hue}, 80%, 80%)`;
+        });
+      },
       products() {
         return this.getProducts;
       }
@@ -80,8 +58,21 @@
     async fetch() {
       await this.fetchProducts();
     },
+    mounted() {
+      const emblaRef = this.$refs.emblaRef
+      const options = { skipSnaps: true }
+
+      this.emblaApi = EmblaCarousel(emblaRef, options)
+    },
     methods: {
-      ...mapActions('products', ['fetchProducts'])
+      ...mapActions('products', ['fetchProducts']),
+      
+      scrollPrev() {
+        this.emblaApi?.scrollPrev()
+      },
+      scrollNext() {
+        this.emblaApi?.scrollNext()
+      }
     }
   };
   </script>
--- store/index.js
+++ store/index.js
@@ -1,50 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import EmblaCarousel from 'embla-carousel'
-import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButtons'
-import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton'
-import Autoplay from 'embla-carousel-autoplay'
-import '../assets/css/base.css'
-import '../assets/css/embla.css'
-
-const OPTIONS = { dragFree: true, loop: true }
-
-const emblaNode = document.querySelector('.embla')
-const viewportNode = emblaNode.querySelector('.embla__viewport')
-const prevBtnNode = emblaNode.querySelector('.embla__button--prev')
-const nextBtnNode = emblaNode.querySelector('.embla__button--next')
-const dotsNode = emblaNode.querySelector('.embla__dots')
-
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Autoplay()])
-
-const onNavButtonClick = (emblaApi) => {
-  const autoplay = emblaApi?.plugins()?.autoplay
-  if (!autoplay) return
-
-  const resetOrStop =
-    autoplay.options.stopOnInteraction === false
-      ? autoplay.reset
-      : autoplay.stop
-
-  resetOrStop()
-}
-
-const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
-  emblaApi,
-  prevBtnNode,
-  nextBtnNode,
-  onNavButtonClick
-)
-const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
-  emblaApi,
-  dotsNode,
-  onNavButtonClick
-)
-
-emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
-emblaApi.on('destroy', removeDotBtnsAndClickHandlers)
-
 Vue.use(Vuex);
 
 const myMiddleware = store => {
