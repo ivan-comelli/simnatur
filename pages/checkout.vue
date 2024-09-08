67afe875480a51358c47313a8beb37887906956f
ivan-comelli <ivancomelli.99@gmail.com>
2024-09-08 18:45:52 -0300

    new

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -6,7 +6,7 @@
         <div class="checkout-area pt-95 pb-100">
             <div class="container">
                 <div class="row" v-if="products.length > 0">
-                    <div class="col-lg-7">
+                    <div id="faq" class="col-lg-7 panel-group">
                         <div class="panel panel-default single-my-account">
                             <div class="panel-heading my-account-title">
                                 <h3 class="panel-title"><span>1 .</span> <a data-bs-toggle="collapse" href="#my-account-1">Tu Informacion Personal</a></h3>
@@ -56,6 +56,45 @@
                                             </div>
                                             <div class="billing-btn">
                                                 <button type="submit">Continuar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default single-my-account">
+                                <div class="panel-heading my-account-title">
+                                    <h3 class="panel-title"><span>3 .</span> <a data-bs-toggle="collapse" href="#my-account-3">Modify your address book entries   </a></h3>
+                                </div>
+                                <div id="my-account-3" class="panel-collapse collapse" data-bs-parent="#faq">
+                                    <div class="panel-body">
+                                        <div class="myaccount-info-wrapper">
+                                            <div class="account-info-wrapper">
+                                                <h4>Address Book Entries</h4>
+                                            </div>
+                                            <div class="entries-wrapper">
+                                                <div class="row">
+                                                    <div class="col-md-6 d-flex align-items-center justify-content-center">
+                                                        <div class="entries-info text-center">
+                                                            <p>Keith L. Castro </p>
+                                                            <p>  559 Pratt Avenue </p>
+                                                            <p> Orchards, WA 98662 </p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6 d-flex align-items-center justify-content-center">
+                                                        <div class="entries-edit-delete text-center">
+                                                            <button class="edit">Edit</button>
+                                                            <button>Delete</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="billing-back-btn">
+                                                <div class="billing-back">
+                                                    <a href="#"><i class="fa fa-arrow-up"></i> back</a>
+                                                </div>
+                                                <div class="billing-btn">
+                                                    <button type="submit">Continue</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
